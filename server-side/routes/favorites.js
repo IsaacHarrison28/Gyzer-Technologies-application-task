a3e49e8ba436004272961df87adea13d518d9a72
Isaac Harrison <sateonlineservices@gmail.com>
2023-11-24 19:42:26 +0200

    successfully resolved database connection issue

--- server-side/routes/favorites.js
+++ server-side/routes/favorites.js
@@ -2,6 +2,20 @@ const Express = require("express");
 const router = Express.Router();
 const favoriteMovie = require("../models/favoritemovie");
 
+router.get("/", (req, res) => {
+  favoriteMovie
+    .find()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        Error: "couldn't get the resources",
+        Message: error,
+      });
+    });
+});
+
 router.post("/", (req, res) => {
   const Data = new favoriteMovie({
     movieTitle: req.body.title,
@@ -14,6 +28,10 @@ router.post("/", (req, res) => {
       res.status(201).json(doc);
     })
     .catch((error) => {
+      res.status(500).json({
+        Error: "Error prevented the operation",
+        Message: error,
+      });
       console.log(error);
     });
 });
